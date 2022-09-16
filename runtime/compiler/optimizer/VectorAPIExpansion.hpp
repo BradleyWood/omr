ab106ab57d245ed924a0d9daeaafd0415e2e3477
Gita Koblents <koblents@ca.ibm.com>
2022-09-15 23:40:20 -0400

    Handle Mask.fromArray() in VectorAPIExpansion

--- runtime/compiler/optimizer/VectorAPIExpansion.hpp
+++ runtime/compiler/optimizer/VectorAPIExpansion.hpp
@@ -136,8 +136,8 @@ class TR_VectorAPIExpansion : public TR::Optimization
    static int32_t const BT_uge = BT_ge | BT_unsigned_compare;
    static int32_t const BT_ult = BT_lt | BT_unsigned_compare;
    static int32_t const BT_ugt = BT_gt | BT_unsigned_compare;
-   
-  /** \brief
+
+   /** \brief
    *  Is passed to methods handlers during analysis and transforamtion phases
    *
    */
@@ -910,8 +910,8 @@ class TR_VectorAPIExpansion : public TR::Optimization
    *
    */
    static TR::Node *testIntrinsicHandler(TR_VectorAPIExpansion *opt, TR::TreeTop *treeTop, TR::Node *node, TR::DataType elementType, TR::VectorLength vectorLength, int32_t numLanes, handlerMode mode);
-   
-  /** \brief
+
+   /** \brief
    *    Scalarizes or vectorizes a node that is a call to \c VectorSupport.unaryOp(),binaryOp(), etc. intrinsic.
    *    In both cases, the node is modified in place.
    *    In the case of scalarization, extra nodes are created(number of lanes minus one)
