98e3c80602eabd7abebf561cd01d3094b48f267b
BradleyWood <bradley.wood@ibm.com>
2022-10-05 12:01:48 -0400

    x86: Support non-vector regs in EVEX
    Signed-off-by: BradleyWood <bradley.wood@ibm.com>

--- compiler/x/amd64/codegen/OMRRealRegister.hpp
+++ compiler/x/amd64/codegen/OMRRealRegister.hpp
@@ -267,7 +267,7 @@ class OMR_EXTENSIBLE RealRegister : public OMR::X86::RealRegister
 
    void setSourceRegisterFieldInEVEX(uint8_t *opcodeByte)
       {
-      uint8_t regNum = getRegisterNumber() - xmm0;
+      uint8_t regNum = ((_fullRegisterBinaryEncodings[_registerNumber].needsRexForByte << 3) | _fullRegisterBinaryEncodings[_registerNumber].id);
       uint8_t bits = 0;
       *opcodeByte &= 0x9F;
 
@@ -286,7 +286,7 @@ class OMR_EXTENSIBLE RealRegister : public OMR::X86::RealRegister
 
    void setSource2ndRegisterFieldInEVEX(uint8_t *opcodeByte)
       {
-      uint8_t regNum = getRegisterNumber() - xmm0;
+      uint8_t regNum = ((_fullRegisterBinaryEncodings[_registerNumber].needsRexForByte << 3) | _fullRegisterBinaryEncodings[_registerNumber].id);
 
       *opcodeByte &= 0x87; // zero out vvvv bits
       *opcodeByte |= (~(regNum << 3)) & 0x78;
@@ -301,7 +301,7 @@ class OMR_EXTENSIBLE RealRegister : public OMR::X86::RealRegister
 
    void setTargetRegisterFieldInEVEX(uint8_t *opcodeByte)
       {
-      uint8_t regNum = getRegisterNumber() - xmm0;
+      uint8_t regNum = ((_fullRegisterBinaryEncodings[_registerNumber].needsRexForByte << 3) | _fullRegisterBinaryEncodings[_registerNumber].id);
       uint8_t bits = 0;
       *opcodeByte &= 0x6F;
 
