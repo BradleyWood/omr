f0a2cc7cd9411b82ab8f353338092c0ea80ec3ba
BradleyWood <bradley.wood@ibm.com>
2023-01-23 13:31:20 -0700

    x86: Implement mToLongBits opcode
    Signed-off-by: BradleyWood <bradley.wood@ibm.com>

--- compiler/x/codegen/OMRCodeGenerator.cpp
+++ compiler/x/codegen/OMRCodeGenerator.cpp
@@ -1063,6 +1063,8 @@ bool OMR::X86::CodeGenerator::getSupportsOpCodeForAutoSIMD(TR::CPU *cpu, TR::ILO
             default:
                return false;
             }
+      case TR::mToLongBits:
+         return cpu->supportsFeature(OMR_FEATURE_X86_AVX512F);
       case TR::b2m:
       case TR::s2m:
       case TR::i2m:
