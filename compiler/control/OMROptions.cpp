2c0890e50053b02eb265d6cba107bbd4a201ca0e
Bradley Wood <bradley.wood@ibm.com>
2024-01-08 10:22:42 -0500

    x86: Enable dual TLH by default
    Signed-off-by: Bradley Wood <bradley.wood@ibm.com>

--- compiler/control/OMROptions.cpp
+++ compiler/control/OMROptions.cpp
@@ -2729,8 +2729,8 @@ OMR::Options::jitPreProcess()
          self()->setOption(TR_DisableZNext);
          }
 
-#if defined(TR_HOST_X86) || defined(TR_HOST_S390) || defined(TR_HOST_ARM64)
-      // Dual TLH disabled by default on X, Z, and AArch64
+#if defined(TR_HOST_S390)
+       // Dual TLH disabled on default on Z
       self()->setOption(TR_DisableDualTLH);
 #endif
 
